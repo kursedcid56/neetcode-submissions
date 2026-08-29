bf41ba859ee4209d9000352d52c0ea67682bed59
kursedcid56 <141313995+kursedcid56@users.noreply.github.com>
2026-08-29 16:43:16 +0700

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+       clean_s = "".join(c.lower() for c in s if c.isalnum())
+       reversed_s = clean_s[::-1]
+       if clean_s == reversed_s:
+            return True
+       else: return False     
